5c1f2e3f0ad7f3df044da0d6e229d35dc5a2c1d4
MarieProksova <marie.milatova@gmail.com>
2021-10-09 15:03:23 +0200

    1 file modified

---
+++ vlastni.js
@@ -0,0 +1,19 @@
+function ukolKalkulacka(cislo1, cislo2) {
+  let cislo1 = Number (prompt ("Zadej první číslo"));
+  let cislo2 = Number (prompt ("Zadej druhé číslo"));
+  
+  let soucet = cislo1 + cislo2;
+  console.log ("Soucet:", soucet);
+  
+  let rozdil = cislo1 - cislo2;
+  console.log ("Rozdil:"rozdil);
+
+  let nasobek = cislo1 * cislo2;
+  console.log ("Nasobek:"nasobek);
+
+  let podil = cislo1 / cislo2;
+  console.log ("Podil:"podil);
+  }
+
+  ukolKalkulacka
+  
