744e009fcb2c250b6f9bb5cfaffa8268573467f9
MarieProksova <marie.milatova@gmail.com>
2021-10-09 14:35:28 +0200

    1 file modified

--- ukol4.js
+++ ukol4.js
@@ -8,11 +8,11 @@ for (let i = cislo1; i<= cislo2; i++) {
 }
 
 //
-let cislo = Number (prompt ("Zadej:"));
-let soucet = 0;
-while (cislo !==0)  {
-    soucet += cislocislo = Number (prompt ("Zadej:"));
-    console.log ("Naakumulovany soucet: " + soucet);
+//let cislo = Number (prompt ("Zadej:"));
+//let soucet = 0;
+//while (cislo !==0)  {
+  //  soucet += cislocislo = Number (prompt ("Zadej:"));
+  // console.log ("Naakumulovany soucet: " + soucet);
 
 }
 
